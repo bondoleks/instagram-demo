55067653664342fa0c0fef73cc562629db2324e1
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2023-02-27 00:17:24 +0200

    Java (Spring) - Form, add, edit, remove

---
+++ src/main/java/com/instagramdemo/instagramDemo/conf/StaticContents.java
@@ -0,0 +1,27 @@
+package com.instagramdemo.instagramDemo.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebMvc
+public class StaticContents implements WebMvcConfigurer {
+
+    private String[][] staticMappings = {
+            {"/static/**", "classpath:/static/"},
+    };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        Arrays.stream(staticMappings)
+                .forEach(mapping ->
+                        registry.addResourceHandler(mapping[0]).addResourceLocations(mapping[1])
+                );
+    }
+}
